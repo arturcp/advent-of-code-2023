ac14b57850db3f2c1eb0e87a903a54caaae8e728
Artur Caliendo Prado <artur.prado@gmail.com>
2023-12-14 09:26:45 -0300

    Solve day 1 part 1 (#1)
    * Solve day 1 part 1
    
    * Add benchee to compare solutions

---
+++ lib/day-1/benchmark.exs
@@ -0,0 +1,9 @@
+alias AdventOfCode2023.Day1.Part1
+# alias AdventOfCode2023.Day1.Part1V2
+alias Benchee
+
+Benchee.run(%{
+  "solution 1" => fn -> Part1.run() end
+  # Here you can add as many solutions as you want
+  # "solution 2" => fn -> Part1V2.run() end
+})
--- lib/input.ex
+++ lib/input.ex
@@ -1,5 +1,10 @@
 defmodule AdventOfCode2023.Input do
-  def read_input(day) do
+  def read_input(_day, input) when not is_nil(input) do
+    input
+    |> String.split("\n", trim: true)
+  end
+
+  def read_input(day, _) do
     File.read!("lib/day-#{day}/input.txt")
     |> String.split("\n", trim: true)
   end
